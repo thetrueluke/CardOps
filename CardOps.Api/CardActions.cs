4f06dbc74d6793e3de3ed901c2f960286c478e6a
Lukas Woojcik <lukas.woojcik@gmail.com>
2025-03-25 17:37:16 +0100

    Added null checks for parameters.

--- CardOps.Api/CardActions.cs
+++ CardOps.Api/CardActions.cs
@@ -11,13 +11,24 @@ namespace CardOps.Api
         [Function(nameof(CardActions))]
         public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users/{userId}/cards/{cardNumber}/actions")] HttpRequest req, string userId, string cardNumber)
         {
-            logger.LogInformation("C# HTTP trigger function processed a request for userId: {UserId}, cardNumber: {CardNumber}", userId, cardNumber);
+            if (string.IsNullOrWhiteSpace(userId))
+            {
+                logger.LogInformation("C# HTTP trigger function processed a request with invalid userId");
+                return new BadRequestObjectResult($"'{nameof(userId)}' cannot be null or whitespace.");
+            }
+            if (string.IsNullOrWhiteSpace(cardNumber))
+            {
+                logger.LogInformation("C# HTTP trigger function processed a request with invalid cardNumber");
+                return new BadRequestObjectResult($"'{nameof(cardNumber)}' cannot be null or whitespace.");
+            }
 
+            logger.LogInformation("C# HTTP trigger function processed a request for userId: {UserId}, cardNumber: {CardNumber}", userId, cardNumber);
+            
             var cardDetails = await cardService.GetCardDetails(userId, cardNumber);
 
             return cardDetails == null
                 ? new NotFoundObjectResult($"Card {cardNumber} not found for user {userId}.")
-                : new OkObjectResult(new Response(ActionDiscoveryService.GetAllowedActions(cardDetails).Select(a => a.Name)));
+                : new OkObjectResult(new Response(cardNumber, userId, ActionDiscoveryService.GetAllowedActions(cardDetails).Select(a => a.Name)));
         }
     }
 }
